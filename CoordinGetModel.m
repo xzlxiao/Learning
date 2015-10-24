ef49668caf4951646c3743026c50fcefeb7ce82e
肖镇龙 <xzlxiao@outlook.com>
2015-10-24 22:34:46 +0800

    change2015-10-24

--- CoordinGetModel.m
+++ CoordinGetModel.m
@@ -1,9 +1,22 @@
 classdef CoordinGetModel < handle
+    % 功能：CoordinGetModel
+    % 输入：无
+    % 属性：stack; CurNum; picread; picRec; Coordination;
+    % 方法：picRead; picRect; resizeRect; Retur; ScrollWheel; 
+    % 事件：CurNumChange; CoorChange;
+    % 备注：肖镇龙/2015-10-24
     properties
         stack;      % 读取的图片
         CurNum; %当前图片的序号
-        Coordination;  % 坐标
+        returncode = [0, 0, 0, 0];
+    end
+    properties(Hidden, SetAccess = protected)
+        % 对象
         picread;  % 图片读取对象
+        picRec;   % imrect对象
+    end
+    properties(Dependent)
+        Coordination;  % 坐标
     end
     events
         CurNumChange;
@@ -12,10 +25,57 @@ classdef CoordinGetModel < handle
     methods 
         function obj = CoordinGetModel()
         end
-        %图片读取函数
-        %坐标框创建函数
-        %坐标框修正函数
-        %返回函数
-        %滚轮函数
+        function picRead(obj, scroll_handle)
+            % 图片读取函数,
+            % （待实现）如果横纵坐标大于某值时，生成缩略图；图片数量大于某值时，生成投影图
+            obj.picread = uiread_picture;
+            pic_waitbar = waitbar(0, '图片读取中...');
+            steps = obj.picread.sizes;
+            for i = 1 : obj.picread.sizes
+                pic_tmp = imread(obj.picread.path_dir{1, i});
+                obj.stack{i} = pic_tmp;
+                waitbar(i/steps, pic_waitbar);
+            end
+            close(pic_waitbar);
+            obj.CurNum = 1;
+            set(scroll_handle, 'value', obj.CurNum, 'Min', 1, 'Max', obj.picread.sizes, 'SliderStep', [1/obj.picread.sizes, 1/obj.picread.sizes]);
+            obj.notify('CurNumChange');
+        end
+        function picRect(obj, axes_handle)
+            % 坐标框创建函数
+            % (待实现）需要添加imrect的限制区域，再次按下按钮时，清除上一个imrect
+            obj.picRec = imrect(axes_handle);
+            addNewPositionCallback(obj.picRec, @obj.resizeRect);
+            fcn = makeConstrainToRectFcn('imrect',get(axes_handle,'XLim'),get(axes_handle,'YLim'));
+            setPositionConstraintFcn(obj.picRec,fcn);
+            obj.notify('CoorChange');
+        end
+        function Coordination = get.Coordination(obj)
+            Coordination = getPosition(obj.picRec);
+        end
+        function resizeRect(obj)
+            % (待实现）坐标框修正函数
+            obj.notify('CoorChange');
+        end
+        function Coordi = Retur(obj)
+            %返回函数
+            Coordi = obj.Coordination;
+        end
+        function ScrollWheel(obj, scroll_handle, data)
+            %滚轮函数
+            if (data.VerticalScrollCount > 0)&&(obj.CurNum < obj.picread.sizes)
+                obj.CurNum = obj.CurNum + 1;
+                set(scroll_handle, 'value', obj.CurNum);
+            elseif (data.VerticalScrollCount < 0)&&(obj.CurNum > 1)
+                obj.CurNum = obj.CurNum - 1;
+                set(scroll_handle, 'value', obj.CurNum);
+            end
+            obj.notify('CurNumChange');
+        end
+        function SliderFcn(obj, scroll_handle)
+            % slider的函数
+            obj.CurNum = round(get(scroll_handle, 'value'));
+            obj.notify('CurNumChange');
+        end
     end
 end
--- CoordinGetView.m
+++ CoordinGetView.m
@@ -6,7 +6,7 @@ classdef CoordinGetView < ViewCenter
         axesPic;
         sliderPic;
         % 坐标显示
-        CoorDisp;
+        % CoorDisp;
         % 按钮
         uitoolbarTop;
         btnInput;
@@ -15,35 +15,61 @@ classdef CoordinGetView < ViewCenter
     end
     properties
         % 对象
-        CoorController;
-        CoorModel;
+        Controller;
+        Model;
     end
     properties
         % 控件属性设置
-        viewtitle;
         CDataInput;
         CDataRect;
         CDataReturn;
     end
     methods
-        function obj = CoordinGetView()
+        function obj = CoordinGetView(Model)
             obj = obj@ViewCenter([500, 400]);
             set(obj.hfig, 'color', [1,1,1],'menubar', 'none','toolbar', 'none', 'name', '坐标获取', 'resize', 'on' );
+            obj.Model = Model;
+            obj.Model.addlistener('CoorChange', @obj.CoordinationUpdate);
+            obj.Model.addlistener('CurNumChange', @obj.CurPicUpdate);
             obj.UIbuild;
+            obj.MakeController;
+            obj.AttachControll(obj.Controller);
         end
         function UIbuild(obj)
-            obj.uipanelAxes = uipanel(obj.hfig, 'units', 'normal', 'pos', [0, 0.1, 1, 0.9]);
+            obj.uipanelAxes = uipanel(obj.hfig, 'units', 'normal', 'pos', [0, 0.02, 1, 0.98]);
             obj.axesPic = axes('parent', obj.uipanelAxes, 'pos', [0.001, 0.001, 0.998, 0.998], 'xTick' , [], 'yTick', [], 'xColor', [1 1 1], 'ycolor', [1 1 1],'visible', 'off');
-            obj.sliderPic = uicontrol(obj.hfig, 'style', 'slider', 'units', 'normal', 'pos', [0, 0.08, 1, 0.02]);
-            obj.CoorDisp = annotation('textbox', [0.1, 0.01, 0.1, 0.05],'edgecolor', [1,1,1]);    % string信息需要加入model中
+            obj.sliderPic = uicontrol(obj.hfig, 'style', 'slider', 'units', 'normal', 'pos', [0, 0, 1, 0.02]);
+            % obj.CoorDisp = annotation('textbox', [0.1, 0.01, 0.1, 0.05],'edgecolor', [1,1,1]);    % string信息需要加入model中
             obj.uitoolbarTop = uitoolbar(obj.hfig);
-            obj.CDataInput = imresize(imread('icons\Open.png'), [16, 16]);
+            obj.CDataInput = imresize(imread('icons/Open.png'), [16, 16]);
             obj.btnInput = uipushtool(obj.uitoolbarTop, 'cdata', obj.CDataInput, 'tooltipstring', '输入图片');  % 回调函数属性为'clickedcallback'
-            obj.CDataRect = imresize(imread('icons\Rect.png'), [16, 16]);
+            obj.CDataRect = imresize(imread('icons/Rect.png'), [16, 16]);
             obj.btnDrawRect = uipushtool(obj.uitoolbarTop, 'cdata', obj.CDataRect, 'tooltipstring', '坐标框');
-            obj.CDataReturn = imresize(imread('icons\return3.png'), [16, 16]);
+            obj.CDataReturn = imresize(imread('icons/return3.png'), [16, 16]);
             obj.CDataReturn = cat(3, obj.CDataReturn, obj.CDataReturn, obj.CDataReturn);
             obj.btnReturn = uipushtool(obj.uitoolbarTop, 'cdata', obj.CDataReturn, 'tooltipstring', '返回');
+        end
+        function CoordinationUpdate(obj, event, data)
+            set(obj.hfig, 'name', mat2str(obj.Model.Coordination));
+        end
+        function CurPicUpdate(obj, event, data)
+            axes(obj.axesPic);
+            imshow(obj.Model.stack{obj.Model.CurNum});
+        end
+        function MakeController(obj)
+            obj.Controller = CoordinGetController(obj, obj.Model);
+        end
+        function AttachControll(obj, controller)
+            cbF = @controller.WheelScrollFcn;
+            set(obj.hfig, 'WindowScrollWheelFcn', cbF);
+            cbF = @controller.sliderPic_callback;
+            set(obj.sliderPic, 'callback', cbF);
+            cbF = @controller.btnInput_callback;
+            set(obj.btnInput, 'clickedcallback', cbF);
+            cbF = @controller.btnDrawRect_callback;
+            set(obj.btnDrawRect, 'clickedcallback', cbF);
+            cbF = @controller.btnReturn_callback;
+            set(obj.btnReturn, 'clickedcallback', cbF);
         end
     end
 end
