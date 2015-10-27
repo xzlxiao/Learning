6fbe7b2eed1f5d16cec73d20f2e70b559c0ebc37
xzlxiao <xzlxiao@outlook.com>
2015-10-27 22:13:08 +0800

    文件转移

--- @PicPreproModel/CreateFolder.m
+++
@@ -1,7 +0,0 @@
-function [] = CreateFolder(obj, dir)
-if ischar(dir) == 0
-    error('地址名必须是字符串');
-end
-if exist(dir) == 0
-mkdir(dir);
-end
--- main_test.m
+++
@@ -1,9 +0,0 @@
-%% Ö÷³ÌÐò²âÊÔ
-clear;clc;
-model1 = CoordinGetModel;
-view1 = CoordinGetView(model1);
-
-%% PicPrecess
-clear;clc;
-a = PicPreproModel;
-b = PicPreproView(a);
